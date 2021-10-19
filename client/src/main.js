f43f649fade80d8e3ecfbb78249bc3a5b909e4e0
jennar305 <71038940+jennar305@users.noreply.github.com>
2021-10-18 20:28:56 -0400

    added notification messages for wrong login

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
 import 'bulma'
-import '@fortawesome/fontawesome-free';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import { Notification, Config } from '@oruga-ui/oruga-next'
+import '@oruga-ui/oruga-next/dist/oruga-full.css'
 
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+.use(Notification)
+.use(Config, {
+    iconPack: 'fas'
+})
+.use(router)
+.mount('#app')
--- client/src/services/session.js
+++ client/src/services/session.js
@@ -1,18 +1,30 @@
 import router from "../router";
 import { Login } from "./users";
+import {NotificationProgrammatic} from "@oruga-ui/oruga-next/dist/esm/notification";
 
 const session = {
     user: null,
-    messages: [],
-    toRoute: '/activity',
+    messages: [],               //{text: string, type: string}
+    toRoute: '/home',
     Login(username, password){
+        try {
+            const response = Login(username, password);
 
-        const response = Login(username, password);
+            this.user = response.user;
 
-        this.user = response.user;
+            router.push(this.toRoute);
 
-        router.push(this.toRoute);
+        } catch (error) {
+            this.messages.push({text: error.msg, type: 'warning'});
+            NotificationProgrammatic.open({
+                duration: 5000,
+                message: error.msg,
+                variant: 'danger',
+                type: 'danger',
+                closable: true,
 
+            })   
+        }
     }
 };
 
